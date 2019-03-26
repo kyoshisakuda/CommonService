8a08e1ea2f05900ef23d783dd4df79adeeaa6975
Kyoshi Sakuda <kyoshi.sakuda@gmail.com>
2019-03-26 17:08:06 -0500

    Comon Service project initialization

---
+++ src/main/java/com/kyoshisakuda/common/CommonService.java
@@ -0,0 +1,13 @@
+package com.kyoshisakuda.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonService.class, args);
+    }
+
+}
